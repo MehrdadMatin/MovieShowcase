428cf4e419e7aea0fcb9559c19cbbde6bf3ce2fc
MehrdadMatin <mehrdad.mpanah@gmail.com>
2023-05-06 13:03:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React from'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import MovieDetail from './components/MovieDetail';
+import SearchResults from './components/SearchResults';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/movie/:movieId" component={MovieDetail} />
+        <Route path="/search" component={SearchResults} />
+      </Switch>
+    </Router>
   );
 }
 
